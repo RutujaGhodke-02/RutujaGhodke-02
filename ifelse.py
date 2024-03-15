3c962a8ff0ef394ebd979ac34ae31bfe41435c0b
RutujaGhodke-02 <aids.31.rutujaghodke@gmail.com>
2024-03-15 17:30:12 +0530

    first commit

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+attendance = int(input("Enter your attendance percentage:"))
+
+if attendance > 75 :
+    print("you are not in defaulter list")
+
+elif 30 < attendance <75 :
+    print("You are in defaulter list")
+
+else:
+    print("You are not Eligible") 
+     
